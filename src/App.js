e7652a26365219d46afcb1495efff755a1e5c6b5
amrgx <amrtarekaly157@gmail.com>
2022-10-12 00:20:36 +0200

    finsh the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Carousel from "./components/Carousel";
+import Destinations from "./components/Destinations";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import { NavBar } from "./components/NavBar";
+import Search from "./components/Search";
+import Selects from "./components/Selects";
 
 function App() {
   return (
     <div >
      <NavBar />
      <Hero />
+     <Destinations />
+     <Search />
+     <Selects />
+     <Carousel />
+     <Footer />
       </div>
   );
 }
